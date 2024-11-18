2fd3eb4be8e15e90f39a8d8f0d3c62cd871b263f
Rencel19 <Rencecorridor19@gmail.com>
2024-11-18 11:23:17 +0800

    Remove unused code in pages/index.vue

--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
-if (!page.value) {
-  throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
-}
+// const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
+// if (!page.value) {
+//   throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true })
+// }
 
 useSeoMeta({
   titleTemplate: '',
-  title: page.value.title,
-  ogTitle: page.value.title,
-  description: page.value.description,
-  ogDescription: page.value.description
+  title: 'Supply Ease',
+  // ogTitle: page.value.title,
+  // description: page.value.description,
+  // ogDescription: page.value.description
 })
 </script>
 
